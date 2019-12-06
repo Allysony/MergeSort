8499d88c0dacd3fc28b2b9a32f2cb4baa60368b2
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2019-12-06 11:59:03 -0800

    mergesort

--- app/proj5.cpp
+++ app/proj5.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include "proj5.hpp"
+#include <iterator>
 
 bool isMinHeap(std::string s) {
     int heapHeight = (s.length() - 2) / 2;
@@ -28,9 +29,66 @@ bool isMinHeap(std::string s) {
     return true;
 }
 
+//
+//template<class Iter>
+//void mergeSort(Iter beg, Iter end)
+//{
+//    auto len = std::distance(beg,end);
+//    if (len < 2)
+//        return;
+//
+//    Iter mid = std::next(beg,len/2);
+//    mergeSort(beg, mid);
+//    mergeSort(mid, end);
+//    std::inplace_merge(beg, mid, end);
+//}
+
+void mergeSort(std::vector<std::string> &vec, std::vector<std::string> &left, std::vector<std::string> &right){
+    int leftSize = left.size();
+    int rightSize = right.size();
+    int i = 0, j = 0, k = 0;
+
+    while (j < leftSize && k < rightSize)
+    {
+        if (left[j] < right[k]) {
+            vec[i] = left[j];
+            j++;
+        }
+        else {
+            vec[i] = right[k];
+            k++;
+        }
+        i++;
+    }
+    while (j < leftSize) {
+        vec[i] = left[j];
+        j++; i++;
+    }
+    while (k < rightSize) {
+        vec[i] = right[k];
+        k++; i++;
+    }
+}
 
 void doSomeSort(std::vector<std::string> &vec) {
-    std::sort(vec.begin(), vec.end());
+    // std::sort(vec.begin(), vec.end());   //  <---- stub stub stub
+    if (vec.size() > 1) {
+
+        int mid = vec.size() / 2;
+        std::vector<std::string> left;
+        std::vector<std::string> right;
+
+        for (int i = 0; i < mid; i++)
+            left.push_back(vec[i]);
+        for (int i = 0; i < (vec.size()) - mid; i++)
+            right.push_back(vec[mid + i]);
+
+        doSomeSort(left);
+        doSomeSort(right);
+        mergeSort(vec, left, right);
+
+    }
+
 
 }
 
@@ -40,7 +98,6 @@ std::vector<std::string> findHeaps(std::istream &in) {
     std::string tmp;
     while (in >> tmp) {
         if(isMinHeap(tmp)){
-
             foo.push_back(tmp);
         }
     }
