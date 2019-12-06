8fa35f602d3644e263120c63e39cc2729a0500bf
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2019-12-06 11:06:24 -0800

    isMinHeap

--- app/proj5.cpp
+++ app/proj5.cpp
@@ -5,21 +5,40 @@
 #include <string>
 #include "proj5.hpp"
 
-bool isMinHeap(std::string s)
-{
-	return true;
+bool isMinHeap(std::string s) {
+    int heapHeight = (s.length() - 2) / 2;
+
+    for (int i = 0; i <= heapHeight; i++) {
+        int leftChild = (int) s[2 * i + 1];
+        int rightChild = (int) s[2 * i + 2];
+        int parent = (int) s[i];
+
+        // left child is not greater
+        if (parent > leftChild) {
+            return false;
+        }
+        // if there is a right child
+        if (2 * i + 2 < s.length()) {
+            // if right child is not greater
+            if (parent > rightChild) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 
-void doSomeSort(std::vector<std::string> & vec)
-{
+void doSomeSort(std::vector<std::string> &vec) {
+
 }
 
 
-std::vector<std::string> findHeaps(std::istream & in)
-{
-	std::vector<std::string> foo;
-
-
-	return foo;
+std::vector<std::string> findHeaps(std::istream &in) {
+    std::vector<std::string> foo;
+    std::string tmp;
+    while (in >> tmp) {
+        foo.push_back(tmp);
+    }
+    return foo;
 }
